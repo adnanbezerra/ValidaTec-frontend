6eddfd2f600ec5cc84251a9a7f02571a0422ba2c
Juan Barezzi <juan.barezzi@gmail.com>
2022-09-21 13:26:16 -0300

    feat: implement button to project details

--- src/components/pages/MyProjectsPage/MyProjectsPage.jsx
+++ src/components/pages/MyProjectsPage/MyProjectsPage.jsx
@@ -4,79 +4,100 @@ import { useNavigate } from "react-router-dom";
 import { BASE_URL } from "../../../mock/data";
 import UserContext from "../../contexts/UserContext";
 import Header from "../../templates/Header/Header";
-import { Container, NoProjectsText, PageTitle, ProjectContainer, ProjectCreators, ProjectDescription, ProjectTitle } from "./MyProjectsStyles";
+import {
+  Container,
+  NoProjectsText,
+  PageTitle,
+  ProjectContainer,
+  ProjectCreators,
+  ProjectDescription,
+  ProjectTitle,
+  ProjectButton,
+} from "./MyProjectsStyles";
 
 export default function MyProjectPage() {
-
-    const { user, setUser } = useContext(UserContext);
-    const [myProjects, setMyProjects] = useState([{
-        id: 1,
-        name: "Como assim mano slk",
-        creators: "Adena, Lele",
-        description: "Esse é um projeot muito legal pq sim",
-        projectPicture: "https://img.a.transfermarkt.technology/portrait/big/353108-1605189960.jpg?lm=1"
+  const { user, setUser } = useContext(UserContext);
+  const [myProjects, setMyProjects] = useState([
+    {
+      id: 1,
+      name: "Como assim mano slk",
+      creators: "Adena, Lele",
+      description: "Esse é um projeot muito legal pq sim",
+      projectPicture:
+        "https://img.a.transfermarkt.technology/portrait/big/353108-1605189960.jpg?lm=1",
     },
     {
-        id: 2,
-        name: "Como assim mano slk",
-        creators: "Adena, Lele",
-        description: "Esse é um projeot muito legal pq sim",
-        projectPicture: "https://img.a.transfermarkt.technology/portrait/big/353108-1605189960.jpg?lm=1"
-    }]);
+      id: 2,
+      name: "Como assim mano slk",
+      creators: "Adena, Lele",
+      description: "Esse é um projeot muito legal pq sim",
+      projectPicture:
+        "https://img.a.transfermarkt.technology/portrait/big/353108-1605189960.jpg?lm=1",
+    },
+  ]);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const userFromStorage = localStorage.getItem('user');
-        if (userFromStorage) {
-            setUser(JSON.parse(userFromStorage));
-            navigate('/my-projects', { replace: true });
-        }
-        else {
-            navigate('/login', { replace: true });
-        }
-
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
-
-    useEffect(() => {
-        axios.get(`${BASE_URL}/patents/${user.name}`)
-            .then(response => {
-                setMyProjects(response.data)
-            })
-            .catch(error => {
-                console.error(error);
-            })
-
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-    function getMyProjects() {
-        return (
-            myProjects.length === 0 ? <NoProjectsText>Sem projetos registrados ainda</NoProjectsText> : myProjects.map(project => getProjectCard(project))
-        )
+  useEffect(() => {
+    const userFromStorage = localStorage.getItem("user");
+    if (userFromStorage) {
+      setUser(JSON.parse(userFromStorage));
+      navigate("/my-projects", { replace: true });
+    } else {
+      navigate("/login", { replace: true });
     }
 
-    function getProjectCard(project) {
-        return (
-            <ProjectContainer onClick={() => navigate(`/project/${project.id}`)}>
-                <img src={project.projectPicture} alt="" />
-                <div style={{display: 'flex', flexDirection: 'column', paddingLeft: '10px'}}>
-                    <ProjectTitle>{project.name}</ProjectTitle>
-                    <ProjectCreators>Criadores: {project.creators}</ProjectCreators>
-                    <ProjectDescription>{project.description}</ProjectDescription>
-                </div>
-            </ProjectContainer>
-        )
-    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}/patents/${user.name}`)
+      .then((response) => {
+        setMyProjects(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  function getMyProjects() {
+    return myProjects.length === 0 ? (
+      <NoProjectsText>Sem projetos registrados ainda</NoProjectsText>
+    ) : (
+      myProjects.map((project) => getProjectCard(project))
+    );
+  }
+
+  function getProjectCard(project) {
     return (
-        <>
-            <Header />
-            <Container>
-                <PageTitle>Seus projetos</PageTitle>
-                {getMyProjects()}
-            </Container>
-        </>
-    )
+      <ProjectContainer onClick={() => navigate(`/project/${project.id}`)}>
+        <img src={project.projectPicture} alt="" />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            paddingLeft: "10px",
+          }}
+        >
+          <ProjectTitle>{project.name}</ProjectTitle>
+          <ProjectCreators>Criadores: {project.creators}</ProjectCreators>
+          <ProjectDescription>{project.description}</ProjectDescription>
+          <ProjectButton>Clique aqui para saber mais</ProjectButton>
+        </div>
+      </ProjectContainer>
+    );
+  }
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <PageTitle>Seus projetos</PageTitle>
+        {getMyProjects()}
+      </Container>
+    </>
+  );
 }
--- src/components/pages/MyProjectsPage/MyProjectsStyles.jsx
+++ src/components/pages/MyProjectsPage/MyProjectsStyles.jsx
@@ -1,63 +1,73 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-top: 20px;
-    z-index: 0;
-    position: absolute;
-    flex-direction: column;
-    z-index: 1;
-`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+  z-index: 0;
+  position: absolute;
+  flex-direction: column;
+  z-index: 1;
+`;
 
 export const PageTitle = styled.p`
-    color: #0b088f;
-    font-size: 45px;
-    font-weight: bold;
-    margin-bottom: 25px;
-`
+  color: #0b088f;
+  font-size: 45px;
+  font-weight: bold;
+  margin-bottom: 25px;
+`;
 
 export const NoProjectsText = styled.p`
-    color: #000;
-    font-weight: bold;
-    font-size: 32px;
-`
+  color: #000;
+  font-weight: bold;
+  font-size: 32px;
+`;
 
 export const ProjectContainer = styled.div`
-    display: flex;
-    width: 80%;
-    border-radius: 10px;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  display: flex;
+  width: 80%;
+  border-radius: 10px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  height: fit-content;
+  padding: 20px;
+  margin-bottom: 20px;
+  z-index: 1;
+
+  img {
+    border-radius: 5px;
+    width: 200px;
     height: fit-content;
-    padding: 20px;
-    margin-bottom: 20px;
-    z-index: 1;
+  }
 
-    img {
-        border-radius: 5px;
-        width: 200px;
-        height: fit-content
-    }
-
-    :hover{
-        cursor: pointer;
-    }
-`
+  :hover {
+    cursor: pointer;
+  }
+`;
 
 export const ProjectTitle = styled.p`
-    font-size: 25px;
-    font-weight: bold;
-`
+  font-size: 25px;
+  font-weight: bold;
+`;
 
 export const ProjectCreators = styled.p`
-    margin-top: 10px;
-    font-size: 15px;
-    color: #424242;
-`
+  margin-top: 10px;
+  font-size: 15px;
+  color: #424242;
+`;
 
 export const ProjectDescription = styled.p`
-    margin-top: 10px;
-    font-size: 20px;
-`
+  margin-top: 10px;
+  font-size: 20px;
+`;
+
+export const ProjectButton = styled.button`
+  border: none;
+  border-radius: 5px;
+  background-color: #00aff0;
+  height: 3rem;
+  width: 12rem;
+  margin-top: 8rem;
+  color: #ffffff;
+`;
