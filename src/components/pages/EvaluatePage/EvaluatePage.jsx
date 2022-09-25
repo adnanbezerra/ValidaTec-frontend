d68a8412e4ffd1b53a26b3bd4f22b1f52cb150fa
Juan Barezzi <juan.barezzi@gmail.com>
2022-09-25 14:06:13 -0300

    fix

--- src/components/pages/EvaluatePage/EvaluatePage.jsx
+++ src/components/pages/EvaluatePage/EvaluatePage.jsx
@@ -55,226 +55,226 @@ export default function EvaluatePage(props) {
     <Container>
       <Header />
       <Title>Avaliando o projeto: {project.name}</Title>
-      <Question>
-        <QuestionTitle>
-          O projeto pode ser facilmente integrado ao ecossistema de inovação e
-          aceito pelo público?
-        </QuestionTitle>
-        <QuestionRadio
-          onChange={(e) => {
-            setAnswer({ ...answer, first: e.target.value });
-          }}
-        >
-          <input type="radio" value="1" id="1" />
-          <label for="1">1</label>
-          <input type="radio" value="2" />
-          <input type="radio" value="3" />
-          <input type="radio" value="4" />
-          <input type="radio" value="5" />
-          <input type="radio" value="6" />
-          <input type="radio" value="7" />
-          <input type="radio" value="8" />
-          <input type="radio" value="9" />
-          <input type="radio" value="10" />
-        </QuestionRadio>
-      </Question>
-      <Question>
-        <QuestionTitle>
-          Quão organizada, competente e qualificada é a liderança do projeto?
-        </QuestionTitle>
-        <QuestionRadio
-          onChange={(e) => {
-            setAnswer({ ...answer, second: e.target.value });
-          }}
-        >
-          <input type="radio" value="1" />
-          <input type="radio" value="2" />
-          <input type="radio" value="3" />
-          <input type="radio" value="4" />
-          <input type="radio" value="5" />
-          <input type="radio" value="6" />
-          <input type="radio" value="7" />
-          <input type="radio" value="8" />
-          <input type="radio" value="9" />
-          <input type="radio" value="10" />
-        </QuestionRadio>
-      </Question>
-      <Question>
-        <QuestionTitle>
-          O quão madura e desenvolvida está a ideia?
-        </QuestionTitle>
-        <QuestionRadio
-          onChange={(e) => {
-            setAnswer({ ...answer, third: e.target.value });
-          }}
-        >
-          <input type="radio" value="1" />
-          <input type="radio" value="2" />
-          <input type="radio" value="3" />
-          <input type="radio" value="4" />
-          <input type="radio" value="5" />
-          <input type="radio" value="6" />
-          <input type="radio" value="7" />
-          <input type="radio" value="8" />
-          <input type="radio" value="9" />
-          <input type="radio" value="10" />
-        </QuestionRadio>
-      </Question>
-      <Question>
-        <QuestionTitle>
-          Quão fácil é tornar a ideia conhecida pelo público alvo? Ela pode ser
-          facilmente viralizada?
-        </QuestionTitle>
-        <QuestionRadio
-          onChange={(e) => {
-            setAnswer({ ...answer, fourth: e.target.value });
-          }}
-        >
-          <input type="radio" value="1" />
-          <input type="radio" value="2" />
-          <input type="radio" value="3" />
-          <input type="radio" value="4" />
-          <input type="radio" value="5" />
-          <input type="radio" value="6" />
-          <input type="radio" value="7" />
-          <input type="radio" value="8" />
-          <input type="radio" value="9" />
-          <input type="radio" value="10" />
-        </QuestionRadio>
-      </Question>
-      <Question>
-        <QuestionTitle>
-          Refere-se a objetividade da implementação da solução tecnológica
-        </QuestionTitle>
-        <QuestionRadio
-          onChange={(e) => {
-            setAnswer({ ...answer, fifth: e.target.value });
-          }}
-        >
-          <input type="radio" value="1" />
-          <input type="radio" value="2" />
-          <input type="radio" value="3" />
-          <input type="radio" value="4" />
-          <input type="radio" value="5" />
-          <input type="radio" value="6" />
-          <input type="radio" value="7" />
-          <input type="radio" value="8" />
-          <input type="radio" value="9" />
-          <input type="radio" value="10" />
-        </QuestionRadio>
-      </Question>
-      <Question>
-        <QuestionTitle>
-          O projeto é bem nichado e tem um público alvo bem definido e
-          facilmente perceptível?
-        </QuestionTitle>
-        <QuestionRadio
-          onChange={(e) => {
-            setAnswer({ ...answer, sixth: e.target.value });
-          }}
-        >
-          <input type="radio" value="1" />
-          <input type="radio" value="2" />
-          <input type="radio" value="3" />
-          <input type="radio" value="4" />
-          <input type="radio" value="5" />
-          <input type="radio" value="6" />
-          <input type="radio" value="7" />
-          <input type="radio" value="8" />
-          <input type="radio" value="9" />
-          <input type="radio" value="10" />
-        </QuestionRadio>
-      </Question>
-      <Question>
-        <QuestionTitle>
-          Quais possíveis parcerias para desenvolvimento da solução tecnológica
-          e conteúdo quantitativo e qualitativo?
-        </QuestionTitle>
-        <QuestionRadio
-          onChange={(e) => {
-            setAnswer({ ...answer, seventh: e.target.value });
-          }}
-        >
-          <input type="radio" value="1" />
-          <input type="radio" value="2" />
-          <input type="radio" value="3" />
-          <input type="radio" value="4" />
-          <input type="radio" value="5" />
-          <input type="radio" value="6" />
-          <input type="radio" value="7" />
-          <input type="radio" value="8" />
-          <input type="radio" value="9" />
-          <input type="radio" value="10" />
-        </QuestionRadio>
-      </Question>
-      <Question>
-        <QuestionTitle>
-          O quão fácil é simplificar o projeto, de modo que ele consuma o mínimo
-          possível, maximizando a taxa de lucro?
-        </QuestionTitle>
-        <QuestionRadio
-          onChange={(e) => {
-            setAnswer({ ...answer, eighth: e.target.value });
-          }}
-        >
-          <input type="radio" value="1" />
-          <input type="radio" value="2" />
-          <input type="radio" value="3" />
-          <input type="radio" value="4" />
-          <input type="radio" value="5" />
-          <input type="radio" value="6" />
-          <input type="radio" value="7" />
-          <input type="radio" value="8" />
-          <input type="radio" value="9" />
-          <input type="radio" value="10" />
-        </QuestionRadio>
-      </Question>
-      <Question>
-        <QuestionTitle>
-          Quais políticas municipais, estaduais e nacionais de incentivo a área
-          de conhecimento da tecnologia? Quais incentivos para desenvolvimento
-          da tecnologia?
-        </QuestionTitle>
-        <QuestionRadio
-          onChange={(e) => {
-            setAnswer({ ...answer, nineth: e.target.value });
-          }}
-        >
-          <input type="radio" value="1" />
-          <input type="radio" value="2" />
-          <input type="radio" value="3" />
-          <input type="radio" value="4" />
-          <input type="radio" value="5" />
-          <input type="radio" value="6" />
-          <input type="radio" value="7" />
-          <input type="radio" value="8" />
-          <input type="radio" value="9" />
-          <input type="radio" value="10" />
-        </QuestionRadio>
-      </Question>
-      <Question>
-        <QuestionTitle>
-          Se a solução proposta tem viabilidade financeira. Quais investimentos
-          financeiro, tempo e RH são viáveis?
-        </QuestionTitle>
-        <QuestionRadio
-          onChange={(e) => {
-            setAnswer({ ...answer, tenth: e.target.value });
-          }}
-        >
-          <input type="radio" value="1" />
-          <input type="radio" value="2" />
-          <input type="radio" value="3" />
-          <input type="radio" value="4" />
-          <input type="radio" value="5" />
-          <input type="radio" value="6" />
-          <input type="radio" value="7" />
-          <input type="radio" value="8" />
-          <input type="radio" value="9" />
-          <input type="radio" value="10" />
-        </QuestionRadio>
-      </Question>
       <form onSubmit={handleEvaluation}>
+        <Question>
+          <QuestionTitle>
+            O projeto pode ser facilmente integrado ao ecossistema de inovação e
+            aceito pelo público?
+          </QuestionTitle>
+          <QuestionRadio
+            onChange={(e) => {
+              setAnswer({ ...answer, first: e.target.value });
+            }}
+          >
+            <input type="radio" value="1" id="1" />
+            <label for="1">1</label>
+            <input type="radio" value="2" />
+            <input type="radio" value="3" />
+            <input type="radio" value="4" />
+            <input type="radio" value="5" />
+            <input type="radio" value="6" />
+            <input type="radio" value="7" />
+            <input type="radio" value="8" />
+            <input type="radio" value="9" />
+            <input type="radio" value="10" />
+          </QuestionRadio>
+        </Question>
+        <Question>
+          <QuestionTitle>
+            Quão organizada, competente e qualificada é a liderança do projeto?
+          </QuestionTitle>
+          <QuestionRadio
+            onChange={(e) => {
+              setAnswer({ ...answer, second: e.target.value });
+            }}
+          >
+            <input type="radio" value="1" />
+            <input type="radio" value="2" />
+            <input type="radio" value="3" />
+            <input type="radio" value="4" />
+            <input type="radio" value="5" />
+            <input type="radio" value="6" />
+            <input type="radio" value="7" />
+            <input type="radio" value="8" />
+            <input type="radio" value="9" />
+            <input type="radio" value="10" />
+          </QuestionRadio>
+        </Question>
+        <Question>
+          <QuestionTitle>
+            O quão madura e desenvolvida está a ideia?
+          </QuestionTitle>
+          <QuestionRadio
+            onChange={(e) => {
+              setAnswer({ ...answer, third: e.target.value });
+            }}
+          >
+            <input type="radio" value="1" />
+            <input type="radio" value="2" />
+            <input type="radio" value="3" />
+            <input type="radio" value="4" />
+            <input type="radio" value="5" />
+            <input type="radio" value="6" />
+            <input type="radio" value="7" />
+            <input type="radio" value="8" />
+            <input type="radio" value="9" />
+            <input type="radio" value="10" />
+          </QuestionRadio>
+        </Question>
+        <Question>
+          <QuestionTitle>
+            Quão fácil é tornar a ideia conhecida pelo público alvo? Ela pode
+            ser facilmente viralizada?
+          </QuestionTitle>
+          <QuestionRadio
+            onChange={(e) => {
+              setAnswer({ ...answer, fourth: e.target.value });
+            }}
+          >
+            <input type="radio" value="1" />
+            <input type="radio" value="2" />
+            <input type="radio" value="3" />
+            <input type="radio" value="4" />
+            <input type="radio" value="5" />
+            <input type="radio" value="6" />
+            <input type="radio" value="7" />
+            <input type="radio" value="8" />
+            <input type="radio" value="9" />
+            <input type="radio" value="10" />
+          </QuestionRadio>
+        </Question>
+        <Question>
+          <QuestionTitle>
+            Refere-se a objetividade da implementação da solução tecnológica
+          </QuestionTitle>
+          <QuestionRadio
+            onChange={(e) => {
+              setAnswer({ ...answer, fifth: e.target.value });
+            }}
+          >
+            <input type="radio" value="1" />
+            <input type="radio" value="2" />
+            <input type="radio" value="3" />
+            <input type="radio" value="4" />
+            <input type="radio" value="5" />
+            <input type="radio" value="6" />
+            <input type="radio" value="7" />
+            <input type="radio" value="8" />
+            <input type="radio" value="9" />
+            <input type="radio" value="10" />
+          </QuestionRadio>
+        </Question>
+        <Question>
+          <QuestionTitle>
+            O projeto é bem nichado e tem um público alvo bem definido e
+            facilmente perceptível?
+          </QuestionTitle>
+          <QuestionRadio
+            onChange={(e) => {
+              setAnswer({ ...answer, sixth: e.target.value });
+            }}
+          >
+            <input type="radio" value="1" />
+            <input type="radio" value="2" />
+            <input type="radio" value="3" />
+            <input type="radio" value="4" />
+            <input type="radio" value="5" />
+            <input type="radio" value="6" />
+            <input type="radio" value="7" />
+            <input type="radio" value="8" />
+            <input type="radio" value="9" />
+            <input type="radio" value="10" />
+          </QuestionRadio>
+        </Question>
+        <Question>
+          <QuestionTitle>
+            Quais possíveis parcerias para desenvolvimento da solução
+            tecnológica e conteúdo quantitativo e qualitativo?
+          </QuestionTitle>
+          <QuestionRadio
+            onChange={(e) => {
+              setAnswer({ ...answer, seventh: e.target.value });
+            }}
+          >
+            <input type="radio" value="1" />
+            <input type="radio" value="2" />
+            <input type="radio" value="3" />
+            <input type="radio" value="4" />
+            <input type="radio" value="5" />
+            <input type="radio" value="6" />
+            <input type="radio" value="7" />
+            <input type="radio" value="8" />
+            <input type="radio" value="9" />
+            <input type="radio" value="10" />
+          </QuestionRadio>
+        </Question>
+        <Question>
+          <QuestionTitle>
+            O quão fácil é simplificar o projeto, de modo que ele consuma o
+            mínimo possível, maximizando a taxa de lucro?
+          </QuestionTitle>
+          <QuestionRadio
+            onChange={(e) => {
+              setAnswer({ ...answer, eighth: e.target.value });
+            }}
+          >
+            <input type="radio" value="1" />
+            <input type="radio" value="2" />
+            <input type="radio" value="3" />
+            <input type="radio" value="4" />
+            <input type="radio" value="5" />
+            <input type="radio" value="6" />
+            <input type="radio" value="7" />
+            <input type="radio" value="8" />
+            <input type="radio" value="9" />
+            <input type="radio" value="10" />
+          </QuestionRadio>
+        </Question>
+        <Question>
+          <QuestionTitle>
+            Quais políticas municipais, estaduais e nacionais de incentivo a
+            área de conhecimento da tecnologia? Quais incentivos para
+            desenvolvimento da tecnologia?
+          </QuestionTitle>
+          <QuestionRadio
+            onChange={(e) => {
+              setAnswer({ ...answer, nineth: e.target.value });
+            }}
+          >
+            <input type="radio" value="1" />
+            <input type="radio" value="2" />
+            <input type="radio" value="3" />
+            <input type="radio" value="4" />
+            <input type="radio" value="5" />
+            <input type="radio" value="6" />
+            <input type="radio" value="7" />
+            <input type="radio" value="8" />
+            <input type="radio" value="9" />
+            <input type="radio" value="10" />
+          </QuestionRadio>
+        </Question>
+        <Question>
+          <QuestionTitle>
+            Se a solução proposta tem viabilidade financeira. Quais
+            investimentos financeiro, tempo e RH são viáveis?
+          </QuestionTitle>
+          <QuestionRadio
+            onChange={(e) => {
+              setAnswer({ ...answer, tenth: e.target.value });
+            }}
+          >
+            <input type="radio" value="1" />
+            <input type="radio" value="2" />
+            <input type="radio" value="3" />
+            <input type="radio" value="4" />
+            <input type="radio" value="5" />
+            <input type="radio" value="6" />
+            <input type="radio" value="7" />
+            <input type="radio" value="8" />
+            <input type="radio" value="9" />
+            <input type="radio" value="10" />
+          </QuestionRadio>
+        </Question>
         <Button>Enviar</Button>
       </form>
     </Container>
